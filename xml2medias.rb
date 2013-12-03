2903bcf24860e824d9a456cc70aeb81b9c0ee649
Code Ass <aycabta@gmail.com>
2013-12-03 16:54:12 +0900

    fix filename detection, objectID_originalname.ext

--- xml2medias.rb
+++ xml2medias.rb
@@ -19,9 +19,16 @@ MEDIA_TAG_TO_EXT = {
 File.open(ARGV[0], "r") do |f|
   xmldoc = REXML::Document.new(f)
   REXML::XPath.each(xmldoc, "//*[@objectID]") do |element|
+    object_id = element.attributes["objectID"]
     ext = MEDIA_TAG_TO_EXT[element.name]
     if not ext.nil?
-      File.open("#{element.attributes["objectID"]}.#{ext}", "w") do |f|
+      export = REXML::XPath.first(xmldoc, "//Export/symbols/Symbol[@objectID=\"#{object_id}\"]")
+      if not export.nil?
+        filename = "#{object_id}_#{export.attributes["name"]}.#{ext}"
+      else
+        filename = "#{object_id}.#{ext}"
+      end
+      File.open(filename, "w") do |f|
         f.write(Base64.decode64(element.elements["data"].elements["data"].text))
       end
     end
